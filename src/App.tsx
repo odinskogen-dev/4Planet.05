4f317fc8098638835b765384116a641b3d0cb79c
odinskogen-dev <odin.skogen@gmail.com>
2026-07-25 20:14:12 +0200

    Load the mobile footer correction

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AppRoutes } from "@/routes/router";
 import { ScrollToTop } from "@/components/ScrollToTop";
 import { ProductNav } from "@/product/ProductNav";
 import "@/styles/global.css";
+import "@/styles/responsive-footer.css";
 
 export default function App() {
   return (
