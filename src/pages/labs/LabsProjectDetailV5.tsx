1a2b3a9e7f96d12a2b46dc94ab7d6536cf6ee1dc
odinskogen-dev <odin.skogen@gmail.com>
2026-08-20 01:28:10 +0200

    LABS: use current Goal Contracts and Leading One metadata

--- src/pages/labs/LabsProjectDetailV5.tsx
+++ src/pages/labs/LabsProjectDetailV5.tsx
@@ -1,7 +1,7 @@
 import LabsProjectDetailPremium from "./LabsProjectDetailPremium";
-import { withProjectMeta } from "./labsProjectMeta";
-import type { LabProject } from "./labsProjection";
+import { withCurrentProjectMeta } from "./labsCurrentMeta";
+import type { LabProject } from "./labsFreshProjection";
 
 export default function LabsProjectDetailV5({ project }: { project: LabProject }) {
-  return <LabsProjectDetailPremium project={withProjectMeta(project)} />;
+  return <LabsProjectDetailPremium project={withCurrentProjectMeta(project)} />;
 }
