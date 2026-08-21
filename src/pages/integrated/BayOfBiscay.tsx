6be67158be546e9cdbf96e3e9437df4ada55ee74
odinskogen-dev <odin.skogen@gmail.com>
2026-08-21 21:18:09 +0200

    Add Bay of Biscay Gold page

---
+++ src/pages/integrated/BayOfBiscay.tsx
@@ -0,0 +1,6 @@
+import { EcosystemGoldExperience } from "@/components/ecosystem/EcosystemGoldExperience";
+import { BAY_OF_BISCAY_GOLD } from "@/ecosystems/bayOfBiscay";
+
+export default function BayOfBiscay() {
+  return <EcosystemGoldExperience profile={BAY_OF_BISCAY_GOLD} />;
+}
