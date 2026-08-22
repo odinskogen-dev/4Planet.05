a8f38763f38f6093eda275a2c65aaf2abf1222d6
odinskogen-dev <odin.skogen@gmail.com>
2026-08-22 13:38:35 +0200

    feat(one-interface): route-ready Amazonia Gold page

---
+++ src/pages/integrated/AmazonRainforestGold.tsx
@@ -0,0 +1,3 @@
+import { EcosystemGoldExperience } from "@/components/ecosystem/EcosystemGoldExperience";
+import { AMAZONIA_GOLD } from "@/ecosystems/amazonia";
+export default function AmazonRainforestGold(){return <EcosystemGoldExperience profile={AMAZONIA_GOLD}/>;}
