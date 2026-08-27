692add83cb638995c594abef920a887a83067902
odinskogen-dev <odin.skogen@gmail.com>
2026-08-27 02:08:52 +0200

    Species: activate source-first mobile hero cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Analytics } from "@/analytics/Analytics";
 import { ProductRouteAnalytics } from "@/analytics/ProductRouteAnalytics";
 import { PublicCompletionBridge } from "@/components/PublicCompletionBridge";
 import "@/styles/global.css";
+import "@/styles/species-source-first-read-v01.css";
 import "@/styles/responsive-footer.css";
 import "@/styles/gold-human-craft.css";
 import "@/styles/premium-completion.css";
